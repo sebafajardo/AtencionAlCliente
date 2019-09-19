0370ace484573074ce396ffecf0b8e352c9d26cb
Sebastian <sebastiangfajardo@gmail.com>
2019-09-18 21:58:32 -0300

    abc

---
+++ AtencionAlCliente/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtencionAlCliente
+{
+    public class Cliente
+    {
+    }
+
+    public class Negocio
+    {
+        private Negocio()
+        {
+          
+
+        }
+
+        public Negocio (string nombre)
+        {
+
+        }
+    }
+
+    public class PuestoAtencion
+    {
+        PuestoAtencion() {
+            numeroActual = 0;
+        }
+
+        PuestoAtencion(Puesto puesto)
+        {
+            numeroActual = 0;
+        }
+
+
+        private static int numeroActual;
+        private Puesto puesto;
+        public enum Puesto
+        {
+            Caja1, Caja2
+        }
+
+
+
+        public bool Atender(Cliente cli)
+        {
+
+        }
+
+        public int NumeroActual
+        {
+            get {
+                return numeroActual;
+            }
+        }
+
+
+    }
+}
